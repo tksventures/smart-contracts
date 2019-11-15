470adaf4b8b92f64fbcc19fdceea3d416a9c6ca6
bravetarget <bravetarget@gmail.com>
2019-11-15 01:21:53 -0700

    Adds Waves RIDE timelock contract and tests #1

---
+++ waves/test/localNode.js
@@ -0,0 +1,42 @@
+const http = require('http');
+
+const localNode = {
+  height() {
+    return new Promise((resolve) => {
+      http.get(
+        {
+          hostname: 'localhost',
+          port: 6869,
+          path: '/blocks/height',
+          agent: false
+        }, 
+        (resp) => {
+          let data = '';
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+        
+          resp.on('end', () => {
+              const { height } = JSON.parse(data)
+              console.log(JSON.parse(data));
+              resolve(height);
+          });
+        }
+      );
+    })
+  },
+
+  waitUntilBlock(block, intervalMs = 1000) {
+    return new Promise((resolve) => {
+      const observer = setInterval(async () => {
+          const height = await localNode.height();
+          if (height >= block) {
+              clearInterval(observer);
+              resolve();
+          }
+      }, intervalMs);
+    });
+  }
+}
+
+module.exports = localNode;
---
+++ waves/test/timelock.ride-test.js
@@ -0,0 +1,260 @@
+const { height, waitUntilBlock } = require('./localNode');
+const wvs = 10 ** 8;
+
+describe('Timelock test suite', async function() {
+    let lockBlock = 0;
+    const totalChunks = 20;
+    const interval = 2;
+    const initialDelay = 2;
+
+    this.timeout((totalChunks + interval + initialDelay) * 21000);
+
+    before(async () => {
+        await setupAccounts({
+            external: 42 * wvs,
+            script: 42 * wvs,
+            owner: 42 * wvs
+        });
+        
+        const script = compile(file('timelock.ride'));
+        const ssTx = setScript({script}, accounts.script);
+        await broadcast(ssTx);
+        await waitForTx(ssTx.id)
+        console.log('Script has been set')
+    });
+
+    describe('Permissions and unlocked state', () => {
+        it('should only allow owner to timelock or withdraw', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: 21 },
+                        { type: 'integer', value: 2 },
+                        { type: 'integer', value: 0 }
+                    ]
+                },
+            }
+            const withdrawTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "withdraw",
+                    args: [{ type: 'integer', value: 42 * wvs }]
+                },
+            }
+    
+            const accessDeniedMessage = "Only the timelock owner can call this function"
+    
+            const accessDeniedTimelock = invokeScript(timelockTxParams, accounts.owner);
+    
+            await expect(broadcast(accessDeniedTimelock)).to.be.rejectedWith(accessDeniedMessage)
+    
+            const accessDeniedWithdrawal = invokeScript(withdrawTxParams, accounts.external);
+    
+            await expect(broadcast(accessDeniedWithdrawal)).to.be.rejectedWith(accessDeniedMessage)
+    
+            const changeOwner = data({
+                data: [
+                    { key: "timelockOwner", value: address(accounts.owner) }
+                ],
+                additionalFee: 0.004 * wvs
+            }, accounts.script);
+    
+            await broadcast(changeOwner);
+            await waitForTx(changeOwner.id);
+    
+            const passesOwnerCheck = invokeScript(timelockTxParams, accounts.owner);
+    
+            await expect(broadcast(passesOwnerCheck)).not.to.be.rejectedWith(accessDeniedMessage)
+        })
+        
+        it('should deposit', async () => {
+            const tx = invokeScript({
+                dApp: address(accounts.script),
+                call: { function: "deposit" },
+                payment: [{assetId: null, amount: 21 * wvs}],
+            }, accounts.owner);
+    
+    
+            await broadcast(tx);
+            await waitForTx(tx.id);
+        })
+    
+        it('should prevent a withdrawal for more than was deposited', async () => {
+            const withdraw = invokeScript({
+                dApp: address(accounts.script),
+                call: {
+                    function: "withdraw",
+                    args: [{type:'integer', value: 42 * wvs}]
+                },
+    
+            }, accounts.owner);
+    
+            expect(broadcast(withdraw)).to.be.rejectedWith("Not enough balance")
+        })
+    
+        it('should withdraw', async () => {
+            const withdraw = invokeScript({
+                dApp: address(accounts.script),
+                call: {
+                    function: "withdraw",
+                    args: [{ type: 'integer', value: 0.9 * wvs }]
+                },
+    
+            }, accounts.owner);
+            await broadcast(withdraw)
+        })
+    })
+
+    describe('Locked state and time management', async () => {
+        it('should prevent locking funds until waves moons', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: totalChunks },
+                        { type: 'integer', value: interval * 4 ** 20},
+                        { type: 'integer', value: initialDelay }
+                    ]
+                },
+            }
+
+            const timelock = invokeScript(timelockTxParams, accounts.owner);
+            expect(broadcast(timelock)).to.be.rejectedWith("This contract has an upper limit of 5 years that funds can be locked")
+        });
+
+        it('should lock the funds', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: totalChunks },
+                        { type: 'integer', value: interval },
+                        { type: 'integer', value: initialDelay }
+                    ]
+                },
+            };
+            const timelock = invokeScript(timelockTxParams, accounts.owner);
+
+            await broadcast(timelock);
+            await waitForTx(timelock.id);
+
+            lockBlock = await height();
+        })
+
+        it('should prevent locking while in a locked state', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: totalChunks },
+                        { type: 'integer', value: interval },
+                        { type: 'integer', value: initialDelay }
+                    ]
+                },
+            };
+            const timelock = invokeScript(timelockTxParams, accounts.owner);
+            expect(broadcast(timelock)).to.be.rejectedWith("Lock already in place")
+        })
+
+        it('should prevent withdrawals until some funds are unlocked', async () => {
+            const withdraw = invokeScript({
+                dApp: address(accounts.script),
+                call: {
+                    function: "withdraw",
+                    args: [{type:'integer', value: 42 * wvs}]
+                },
+    
+            }, accounts.owner);
+    
+            expect(broadcast(withdraw)).to.be.rejectedWith("All funds are currently locked")
+        });
+
+        it('should withdraw only unlocked funds', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: totalChunks },
+                        { type: 'integer', value: interval },
+                        { type: 'integer', value: initialDelay }
+                    ]
+                },
+            };
+
+            const withdrawParams = {
+                dApp: address(accounts.script),
+                call: {
+                    function: "withdraw",
+                    args: [{type:'integer', value: 42 * wvs}]
+                },
+    
+            };
+
+            const earlyWithdraw = invokeScript(withdrawParams, accounts.owner);
+            await expect(broadcast(earlyWithdraw)).to.be.rejectedWith("All funds are currently locked")
+
+            await waitUntilBlock(lockBlock + interval + initialDelay);
+
+            const untappedWithdraw = invokeScript(withdrawParams, accounts.owner);
+
+            await broadcast(untappedWithdraw);
+            await waitForTx(untappedWithdraw.id);
+
+            const exhaustedWithdraw = invokeScript(withdrawParams, accounts.owner);
+            await expect(broadcast(exhaustedWithdraw)).to.be.rejectedWith("All funds are currently locked")
+
+            const timelock = invokeScript(timelockTxParams, accounts.owner);
+            expect(broadcast(timelock)).to.be.rejectedWith("Lock already in place")
+        });
+
+        it('should unlock after designated time', async () => {
+            const timelockTxParams = {
+                dApp: address(accounts.script),
+                call: { 
+                    function: "timelock",
+                    args: [
+                        { type: 'integer', value: totalChunks },
+                        { type: 'integer', value: interval },
+                        { type: 'integer', value: initialDelay }
+                    ]
+                },
+            };
+
+            const withdrawParams = {
+                dApp: address(accounts.script),
+                call: {
+                    function: "withdraw",
+                    args: [{type:'integer', value: 42 * wvs}]
+                },
+    
+            };
+
+            await waitUntilBlock(lockBlock + (interval * totalChunks) + initialDelay);
+
+            const finalWithdrawal = invokeScript(withdrawParams, accounts.owner);
+
+            const { trace } = await broadcast(finalWithdrawal);
+            await waitForTx(finalWithdrawal.id);
+
+            const [ unlock ] = trace; 
+            const { data } = unlock.result;
+            const [ stateChanges ] = data;
+
+            expect(stateChanges.key).to.equal('locked');
+            expect(stateChanges.value).to.equal('false');
+
+            const exhaustedWithdraw = invokeScript(withdrawParams, accounts.owner);
+            await expect(broadcast(exhaustedWithdraw)).to.be.rejectedWith("Not enough balance")
+
+            const timelock = invokeScript(timelockTxParams, accounts.owner);
+            expect(broadcast(timelock)).to.be.rejectedWith("There are not enough funds to lock")
+        });
+    });
+    
+})
