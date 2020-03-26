261f4ea36dee2cf2f2ee0afb30a8e2b861a4cffe
bravetarget <bravetarget@gmail.com>
2020-03-26 12:48:46 -0600

    Adds serverless page and dapp bootstrapper

---
+++ waves/timelock/bootstrap.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const seeds = {
+  script: "waves tokes timelock dapp test three",
+  testRich: "waves private node seed with waves tokens",
+  testIssuer: "issue some test tokens issue some test tokens",
+  testOwner: "timelock test owner timelock test owner",
+  testMisc: "misc handler misc handler misc handler misc handler",
+  main: "test main test main test main test main"
+}
+let tokenAssetId = null;
+const wvs = 10 ** 8;
+
+async function bootstrap() {
+  for (let x in seeds){
+    await fundSeed(seeds[x]);
+  }
+  tokenAssetId = await issueToken('Test Tokes', 100000 * wvs, seeds.testIssuer);
+}
+
+async function issueToken(name, quantity, seed) {
+  const signedIssueTx = issue({
+    name,
+    quantity,
+    description: 'test token',
+  }, seed);
+  const result = await broadcast(signedIssueTx);
+  const conf = await waitForTx(signedIssueTx.id);
+
+  console.log(result, conf);
+
+  fs.writeFileSync('asset.db', signedIssueTx.id);
+  return signedIssueTx.id;
+}
+
+async function fundSeed(seed) {
+  const tx = transfer({
+    recipient: address(seed),
+    amount: 42 * wvs,
+  }, seeds.testRich);
+
+  const result = await broadcast(tx);
+  const conf = await waitForTx(tx.id);
+
+  console.log(result, conf);
+}
+
+async function run() {
+  await bootstrap();
+}
+
+run();
