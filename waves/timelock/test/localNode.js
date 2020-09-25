c100c7ac2c4438d754f492c50a6193b8f536e308
bravetarget <bravetarget@gmail.com>
2020-09-25 16:49:34 -0600

    Updated unit tests, minor cleanup #7

--- waves/timelock/test/localNode.js
+++ waves/timelock/test/localNode.js
@@ -8,35 +8,60 @@ const localNode = {
           hostname: 'localhost',
           port: 6869,
           path: '/blocks/height',
-          agent: false
-        }, 
+          agent: false,
+        },
         (resp) => {
           let data = '';
           resp.on('data', (chunk) => {
             data += chunk;
           });
-        
+
           resp.on('end', () => {
-              const { height } = JSON.parse(data)
-              console.log(JSON.parse(data));
-              resolve(height);
+            const { height } = JSON.parse(data);
+            console.log(JSON.parse(data));
+            resolve(height);
           });
-        }
+        },
       );
-    })
+    });
   },
 
   waitUntilBlock(block, intervalMs = 1000) {
     return new Promise((resolve) => {
       const observer = setInterval(async () => {
-          const height = await localNode.height();
-          if (height >= block) {
-              clearInterval(observer);
-              resolve();
-          }
+        const height = await localNode.height();
+        if (height >= block) {
+          clearInterval(observer);
+          resolve();
+        }
       }, intervalMs);
     });
-  }
-}
+  },
+
+  accountData(address, key) {
+    return new Promise((resolve) => {
+      http.get(
+        {
+          hostname: 'localhost',
+          port: 6869,
+          path: `/addresses/data/${address}/${key}`,
+          agent: false,
+        },
+        (resp) => {
+          let data = '';
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+
+          resp.on('end', () => {
+            const { value } = JSON.parse(data);
+            console.log(JSON.parse(data));
+            resolve(value);
+          });
+        },
+      );
+    });
+  },
+};
 
 module.exports = localNode;
